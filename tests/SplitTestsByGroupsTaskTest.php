485a9edf9bcc39a764d0a92db5b8d16a0c638f19
Davert <davert.php@resend.cc>
2016-10-23 02:41:32 +0300

    fixed travis tests

--- tests/SplitTestsByGroupsTaskTest.php
+++ tests/SplitTestsByGroupsTaskTest.php
@@ -7,7 +7,7 @@ class SplitTestsByGroupsTaskTest extends PHPUnit_Framework_TestCase
     public function testGroupsCanBeSplit()
     {
         $task = new Codeception\Task\SplitTestsByGroupsTask(10);
-        $task->testsFrom(realpath(__DIR__.'/../vendor/codeception/base/tests/unit/Codeception/Command'))
+        $task->testsFrom('vendor/codeception/base/tests/unit/Codeception/Command')
             ->groupsTo('tests/result/group_')
             ->run();
 
@@ -19,8 +19,8 @@ class SplitTestsByGroupsTaskTest extends PHPUnit_Framework_TestCase
     public function testSplitFilesByGroups()
     {
         $task = new Codeception\Task\SplitTestsByGroupsTask(5);
-        $task->testsFrom(realpath(__DIR__.'/../vendor/codeception/base/tests/unit/Codeception/Command'))
-            ->projectRoot(realpath(__DIR__.'/../vendor/codeception/base/'))
+        $task->testsFrom('vendor/codeception/base/tests/unit/Codeception/Command')
+            ->projectRoot('vendor/codeception/base/')
             ->groupsTo('tests/result/group_')
             ->run();
 
