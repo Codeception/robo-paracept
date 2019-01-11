d2474921f3a703741b570fbe1f2a96dacbafd191
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2019-01-11 09:33:28 +0100

    Be aware of test dependency during split

--- src/SplitTestsByGroups.php
+++ src/SplitTestsByGroups.php
@@ -13,11 +13,21 @@ use Symfony\Component\Finder\SplFileInfo;
 
 trait SplitTestsByGroups
 {
+    /**
+     * @param $numGroups
+     *
+     * @return SplitTestsByGroupsTask
+     */
     protected function taskSplitTestsByGroups($numGroups)
     {
         return $this->task(SplitTestsByGroupsTask::class, $numGroups);
     }
 
+    /**
+     * @param $numGroups
+     *
+     * @return SplitTestFilesByGroupsTask
+     */
     protected function taskSplitTestFilesByGroups($numGroups)
     {
         return $this->task(SplitTestFilesByGroupsTask::class, $numGroups);
@@ -64,6 +74,72 @@ abstract class TestsSplitter extends BaseTask
 
         return $this;
     }
+
+    /**
+     * @param       $item
+     * @param array $items
+     * @param array $resolved
+     * @param array $unresolved
+     *
+     * @return array
+     */
+    protected function resolveDependencies($item, array $items, array $resolved, array $unresolved) {
+        $unresolved[] = $item;
+        foreach ($items[$item] as $dep) {
+            if (!in_array($dep, $resolved)) {
+                if (!in_array($dep, $unresolved)) {
+                    $unresolved[] = $dep;
+                    list($resolved, $unresolved) = $this->resolveDependencies($dep, $items, $resolved, $unresolved);
+                } else {
+                    throw new \RuntimeException("Circular dependency: $item -> $dep");
+                }
+            }
+        }
+        // Add $item to $resolved if it's not already there
+        if (!in_array($item, $resolved)) {
+            $resolved[] = $item;
+        }
+        // Remove all occurrences of $item in $unresolved
+        while (($index = array_search($item, $unresolved)) !== false) {
+            unset($unresolved[$index]);
+        }
+
+        return [$resolved, $unresolved];
+    }
+
+    /**
+     * Make sure that tests are in array are always with full path and name.
+     *
+     * @param array $testsListWithDependencies
+     *
+     * @return array
+     */
+    protected function resolveDependenciesToFullNames(array $testsListWithDependencies){
+        // make sure that dependencies are in array as full names
+        foreach ($testsListWithDependencies as $testName => $test) {
+            foreach ($test as $i => $dependency) {
+
+                // just test name, that means that class name is the same, just different method name
+                if (strpos($dependency, ':') === false) {
+                    $testsListWithDependencies[$testName][$i] = substr($testName,0,strpos($testName, ':') + 1) . $dependency;
+                    continue;
+                }
+
+                // className:testName, that means we need to find proper test.
+                list($targetTestFileName, $targetTestMethodName) = explode(':', $dependency);
+                // look for proper test in list of all tests. Test could be in different directory so we need to compare
+                // strings and if matched we just assign found test name
+                foreach (array_keys($testsListWithDependencies) as $arrayKey) {
+                    if (strpos($arrayKey, $targetTestFileName . '.php:' . $targetTestMethodName) !== false) {
+                        $testsListWithDependencies[$testName][$i] = $arrayKey;
+                        continue 2;
+                    }
+                }
+                throw new \RuntimeException('Dependency target test '.$dependency.' not found. Please make sure test exists and you are using full test name');
+            }
+        }
+        return $testsListWithDependencies;
+    }
 }
 
 /**
@@ -89,19 +165,78 @@ class SplitTestsByGroupsTask extends TestsSplitter implements TaskInterface
         $testLoader->loadTests($this->testsFrom);
         $tests = $testLoader->getTests();
 
-        $i = 0;
-        $groups = [];
-
         $this->printTaskInfo('Processing ' . count($tests) . ' tests');
-        // splitting tests by groups
+
+        $testsHaveAtLeastOneDependency = false;
+
+        // test preloading (and fetching dependencies) requires dummy DI service.
+        $di = new \Codeception\Lib\Di();
+        // gather test dependencies and deal with dataproviders
+        $testsListWithDependencies = [];
         foreach ($tests as $test) {
             if ($test instanceof DataProvider || $test instanceof DataProviderTestSuite) {
                 $test = current($test->tests());
             }
-            $groups[($i % $this->numGroups) + 1][] = TestDescriptor::getTestFullName($test);
-            $i++;
+
+            // load dependencies for cest type. Unit tests dependencies are loaded automatically
+            if ($test instanceof \Codeception\Test\Cest) {
+                $test->getMetadata()->setServices(['di'=>$di]);
+                $test->preload();
+            }
+
+            $testsListWithDependencies[TestDescriptor::getTestFullName($test)] = $test->getMetadata()->getDependencies();
+            if ($testsHaveAtLeastOneDependency === false and count($test->getMetadata()->getDependencies()) != 0) {
+                $testsHaveAtLeastOneDependency = true;
+            }
         }
 
+        if ($testsHaveAtLeastOneDependency) {
+            $this->printTaskInfo('Resolving test dependencies');
+            // make sure that dependencies are in array as full names
+            try {
+                $testsListWithDependencies = $this->resolveDependenciesToFullNames($testsListWithDependencies);
+            } catch (\Exception $e) {
+                $this->printTaskError($e->getMessage());
+                exit;
+            }
+
+            // resolved and ordered list of dependencies
+            $orderedListOfTests = [];
+            // helper array
+            $unresolved = [];
+
+            // Resolve dependencies for each test
+            foreach (array_keys($testsListWithDependencies) as $test) {
+                try {
+                    list ($orderedListOfTests, $unresolved) = $this->resolveDependencies($test, $testsListWithDependencies, $orderedListOfTests, $unresolved);
+                } catch (\Exception $e) {
+                    $this->printTaskError($e->getMessage());
+                    exit;
+                }
+            }
+
+        // if we don't have any dependencies just use keys from original list.
+        } else {
+            $orderedListOfTests = array_keys($testsListWithDependencies);
+        }
+
+        // for even split, calculate number of tests in each group
+        $numberOfElementsInGroup = ceil(count($orderedListOfTests) / $this->numGroups);
+
+        $i = 1;
+        $groups = [];
+
+        // split tests into files.
+        foreach ($orderedListOfTests as $test) {
+            // move to the next group ONLY if number of tests is equal or greater desired number of tests in group
+            // AND current test has no dependencies AKA: we  are in different branch than previous test
+            if (!empty($groups[$i]) AND count($groups[$i]) >= $numberOfElementsInGroup AND empty($testsListWithDependencies[$test])) {
+                $i++;
+            }
+
+            $groups[$i][] = $test;
+        }
+        
         // saving group files
         foreach ($groups as $i => $tests) {
             $filename = $this->saveTo . $i;
