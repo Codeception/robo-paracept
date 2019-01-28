09bfede69fa4f6aef094c0bbfbd3f5c1b799e50b
Grzegorz Drozd <grzegorz.drozd@gmail.com>
2019-01-28 21:15:03 +0100

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 //require_once 'vendor/autoload.php';
-//require_once 'vendor/codeception/codeception/autoload.php';
+require_once 'vendor/codeception/codeception/autoload.php';
 
 \Robo\Robo::createDefaultContainer(
     new Symfony\Component\Console\Input\ArrayInput([]),
