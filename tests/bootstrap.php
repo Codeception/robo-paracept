ff00b078921ddd072122cd5e8267fdb76f92d054
Grzegorz Drozd <1885137+GrzegorzDrozd@users.noreply.github.com>
2019-02-15 22:18:06 +0100

    Be aware of test dependency during split (#46)
    * Be aware of test dependency during split
    
    * Test fixes
    New tests
    Test fixtures
    
    * Travis fixes
    
    * add verbose to see why tests fail
    
    * Build fix
    
    * still fails :/
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * dev option
    
    * dev option
    
    * test what file is used
    
    * test what file is used
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * temp disable some tests
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * remove debug
    
    * restore minimal version of codeception dependency
    
    * restore minimal version of codeception dependency with php 7.0
    
    * fix test cases

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+//require_once 'vendor/autoload.php';
+require_once 'vendor/codeception/codeception/autoload.php';
+
 \Robo\Robo::createDefaultContainer(
     new Symfony\Component\Console\Input\ArrayInput([]),
     new Symfony\Component\Console\Output\NullOutput()
