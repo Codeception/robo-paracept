698f67ac7127cc2ef62367fde9049b61e0115de9
KJunker <junker.kurt@googlemail.com>
2021-06-19 22:02:50 +0200

    fixing bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
-//require_once 'vendor/autoload.php';
-require_once 'vendor/codeception/codeception/autoload.php';
 
-\Robo\Robo::createDefaultContainer(
-    new Symfony\Component\Console\Input\ArrayInput([]),
-    new Symfony\Component\Console\Output\NullOutput()
-);
-//\Robo\Robo::getContainer()->add('logger', new \Consolidation\Log\Logger(new \Symfony\Component\Console\Output\NullOutput));
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Robo\Robo;
+
+Robo::createContainer();
