d591380e53e2d6ba7b8b46e5dde457bd2f8166ef
KJunker <junker.kurt@googlemail.com>
2021-07-12 22:50:09 +0200

    Fixing configs of composer and phpunit Tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,18 @@
 <?php
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/vendor/codeception/codeception/autoload.php';
 
 use Robo\Robo;
 
 const TEST_PATH = __DIR__;
 
+if (!is_dir($concurrentDirectory = TEST_PATH . '/result/')
+    && !mkdir($concurrentDirectory)
+    && !is_dir($concurrentDirectory)
+) {
+    throw new \RuntimeException(
+        sprintf('Directory "%s" was not created', $concurrentDirectory)
+    );
+}
 Robo::createContainer();
