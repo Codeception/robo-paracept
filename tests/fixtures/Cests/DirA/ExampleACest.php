58ad08f5f4499336bf5a2815bdb343a853ff2d97
KJunker <junker.kurt@googlemail.com>
2021-07-14 01:03:42 +0200

    Feature/refactor codeception robo (#56)
    * Follow links when splitting test files
    
    This seems appropriate to me since it matches the default codeception behaviour.  This was motivated because I was surprised that tests were missing after splitting when codeception would have included them.
    
    * Split Task for newer Phpunit
    
    **Context**
    
    Phpunit has restructure its classes with namespace starting from
    version 6 and this breaks the `DataProvider` test splitting.
    
    This update will allow it to work for both old and new Phpunit.
    
    * Be aware of test dependency during split (#46)
    
    * Be aware of test dependency during split
    
    * Test fixes
    New tests
    Test fixtures
    
    * Travis fixes
    
    * add verbose to see why tests fail
    
    * Build fix
    
    * still fails :/
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * Test if correct code is executed.
    
    * dev option
    
    * dev option
    
    * test what file is used
    
    * test what file is used
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * temp disable some tests
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * remove debug
    
    * restore minimal version of codeception dependency
    
    * restore minimal version of codeception dependency with php 7.0
    
    * fix test cases
    
    * Split ReportMerger into single Classes
    
    * Split TestSplitter into single Classes
    
    * Updating composer
    
    * fixing bootstrap
    
    * Moved Files from src to src/Splitter
    
    * Created Test for TestFileSplitterTask
    
    * added TEST_PATH const
    
    * updated composer autoload-dev
    
    * updated composer autoload-dev
    fixed php version
    fixed .travis.yml php version
    
    * Fixed TestFileSplitterTaskTest.php because of deprecated methods
    
    * Tests for TestsSplitterTask and refactoring
    
    * CodeStyle and TypeError fix, Test for XmlReportMergerTask
    
    * deleted outdated files
    
    * refactoring fixtures
    
    * Correction of Tests Path
    
    * Fixing configs of composer and phpunit Tests
    
    * added 3 tests for Splitting
    
    * Updated TestsSplitter.php because of newer PHPUnit Version
    
    * fixed return type
    
    * Splitting Tests with newer PHPUnit Version
    
    * added TearDown to clear results
    
    * updated composer.json, remove explicit using of ExecutionOrderDependency (comes with PHPUnit 9.3.0)
    
    * updated README.md
    
    * updated php version on composer and before_script in .travis.yml
    
    * updated composer.lock with new php Version
    
    Co-authored-by: Michael Bodnarchuk <DavertMik@users.noreply.github.com>
    Co-authored-by: hirowatari <Kenton@hirowatari.ca>
    Co-authored-by: Taufek Johar <taufek@gmail.com>
    Co-authored-by: Grzegorz Drozd <1885137+GrzegorzDrozd@users.noreply.github.com>

---
+++ tests/fixtures/Cests/DirA/ExampleACest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Classes\DirA;
+
+use Codeception\Test\Unit;
+
+class ExampleACest extends Unit
+{
+    public function testExampleGoTo()
+    {
+        $this->assertTrue(false);
+    }
+}
