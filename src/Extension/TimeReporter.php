8a7e133547eee4dad58b7947c4cfed18ff78993f
Joe Rieger <JoeRieger@rocketmortgage.com>
2022-04-22 08:21:17 -0400

    bug/103 - Fix bug where leading character of a relative test name was being removed erroneously

--- src/Extension/TimeReporter.php
+++ src/Extension/TimeReporter.php
@@ -45,6 +45,8 @@ class TimeReporter extends Extension
     public function getTestName(TestEvent $e): string
     {
         $name = Descriptor::getTestFullName($e->getTest());
-        return substr(str_replace($this->getRootDir(), '', $name), 1);
+
+        // If a leading DIRECTORY_SEPARATOR was found, remove it
+        return ltrim(str_replace($this->getRootDir(), '', $name), DIRECTORY_SEPARATOR);
     }
 }
